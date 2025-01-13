c526e42ef067e72805c6c536320a85fafc885d1c
blackwhale.eth <victor.blackwhale@gmail.com>
2025-01-13 14:44:44 +0100

    chore (widget): change from swing to lifi for swaps

--- src/components/app/widget/SwapWidget.tsx
+++ src/components/app/widget/SwapWidget.tsx
@@ -1,10 +1,14 @@
-import "@swing.xyz/ui/theme.css";
-import { Swap } from "@swing.xyz/ui";
+import { LiFiWidget, WidgetConfig } from "@lifi/widget";
 
-export function SwapWidget() {
-  return (
-    <div className="w-[90vw] max-w-[800px] mx-auto sm:w-[95vw] xs:w-screen xs:px-2.5 pb-8">
-      <Swap projectId="swing-bridge" environment="production" />
-    </div>
-  );
-}
+const widgetConfig: Omit<WidgetConfig, "integrator"> = {
+  theme: {
+    container: {
+      border: "1px solid rgb(234, 234, 234)",
+      borderRadius: "16px",
+    },
+  },
+};
+
+export const SwapWidget = () => {
+  return <LiFiWidget integrator="Ledgity" config={widgetConfig} />;
+};
