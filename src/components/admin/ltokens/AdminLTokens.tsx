38687a540997f16d704448de02c8a84fca244969
blackwhale.eth <victor.blackwhale@gmail.com>
2025-02-25 21:10:29 +0100

    fix (comp): err on chains with no ltokens

--- src/components/admin/ltokens/AdminLTokens.tsx
+++ src/components/admin/ltokens/AdminLTokens.tsx
@@ -6,37 +6,50 @@ import {
   SelectValue,
   TokenLogo,
 } from "@/components/ui";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { AdminLToken } from "./AdminLToken";
 // Context
 import { useAppDataContext } from "@/hooks/context/AppDataContextProvider";
 
 export function AdminLTokens() {
   const { lTokenInfosCurrentChain } = useAppDataContext();
-  const [lToken, setLToken] = useState(lTokenInfosCurrentChain[0]?.name);
+  const [lTokenSymbol, setLTokenSymbol] = useState(
+    lTokenInfosCurrentChain[0]?.symbol,
+  );
+
+  useEffect(() => {
+    if (lTokenInfosCurrentChain.length) {
+      setLTokenSymbol(lTokenInfosCurrentChain[0].symbol);
+    }
+  }, [lTokenInfosCurrentChain]);
 
   return (
     <section className="flex flex-col gap-6 justify-center items-center">
-      <Select
-        onValueChange={(value: string) => setLToken(value)}
-        value={lToken}
-      >
-        <SelectTrigger>
-          <SelectValue placeholder="No L-Tokens available" />
-        </SelectTrigger>
-        <SelectContent>
-          {lTokenInfosCurrentChain.map((token) => (
-            <SelectItem value={token.name} key={token.name}>
-              <div className="flex justify-center items-center gap-[0.6rem]">
-                <TokenLogo symbol={token.name} size={28} />
-                <p className="font-semibold">{token.name}</p>
-              </div>
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
-
-      <AdminLToken lTokenSymbol={lToken} />
+      {lTokenInfosCurrentChain.length ? (
+        <>
+          <Select
+            onValueChange={(value: string) => setLTokenSymbol(value)}
+            value={lTokenSymbol}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="No L-Tokens available" />
+            </SelectTrigger>
+            <SelectContent>
+              {lTokenInfosCurrentChain.map((token) => (
+                <SelectItem key={token.symbol} value={token.symbol}>
+                  <div className="flex justify-center items-center gap-[0.6rem]">
+                    <TokenLogo symbol={token.symbol} size={28} />
+                    <p className="font-semibold">{token.symbol}</p>
+                  </div>
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <AdminLToken lTokenSymbol={lTokenSymbol} />
+        </>
+      ) : (
+        <p>No L-Tokens available</p>
+      )}
     </section>
   );
 }
